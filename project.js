0631f820b7018f02063ed5c65838f87de79503ca
Ben Larrabee <ben.larrabee@interapthq.com>
2016-09-22 11:07:40 -0400

    Thursday, 10:35

--- project.js
+++ project.js
@@ -1,32 +1,35 @@
+// Variable Declaration and Related Functions
 
-// id = "weather-request" is attached to the form
-// Louisa is at         "location": {
-//          "lat": 38.0643297,
-//          "lng": -82.6267345
+var gFront = "https://maps.googleapis.com/maps/api/geocode/json?";
+var gBack = "&key=AIzaSyC3EFAGe9WBrT4FeTfw7X3P4LyVvQXV_Hw";
 
-var google = "https://maps.googleapis.com/maps/api/geocode/json?"+"&key=AIzaSyC3EFAGe9WBrT4FeTfw7X3P4LyVvQXV_Hw";//lacking address in middle
+var louisa ="https://api.darksky.net/forecast/c0188a72d861db401edede8a318711f6/38.0634,-82.6267";
 
-var louisa /*darkSky */ ="https://api.darksky.net/forecast/c0188a72d861db401edede8a318711f6/38.0634,-82.6267";//lacking Lat,Lng at the end.
+var newLocation ="https://api.darksky.net/forecast/c0188a72d861db401edede8a318711f6/89.5,0";
 
 function round(num){
-  return Math.floor(num + 0.5);
+  return Math.round(num);
 }
+
 function toPercent(num){
-  if(!num){
-    return 0;
-  }
+  if(!num) { return 0; }
   return Math.floor((num*100)+0.5);
 }
 
-$(function(){
-
-//function getWeather(url=louisa){
-  $.ajax(louisa, {dataType: "jsonp"}).done(function(data){
+function getWeather(url) {
+  $.ajax( url, {dataType: "jsonp"} ).done(function(data){
     $("#today-currentTemp").html(round(data.currently.temperature));
     $("#today-summary").html(data.currently.summary);
     $("#today-minTemp").html(round(data.daily.data[0].temperatureMin));
     $("#today-maxTemp").html(round(data.daily.data[0].temperatureMax));
     $("#today-chanceRain").html(toPercent(data.daily.data[0].precipProbability));
+  });
+}
+
+// -------------------------------------------------
+$(function(){
+
+  getWeather(louisa);
 
 
     $("button").click(function(){
@@ -40,15 +43,16 @@ $(function(){
         $(".location").html(loca.results[0].address_components[1].long_name);
         $(".location").append(" , "+loca.results[0].address_components[2].short_name);
 
-        var newLocation = "https://api.darksky.net/forecast/c0188a72d861db401edede8a318711f6/" + lat +"," + lng;
-        console.log(newLocation);
-          $.ajax(newLocation, {dataType: "jsonp"}).done(function(data){
-            $("#today-currentTemp").html(round(data.currently.temperature));
-            $("#today-summary").html(data.currently.summary);
-            $("#today-minTemp").html(round(data.daily.data[0].temperatureMin));
-            $("#today-maxTemp").html(round(data.daily.data[0].temperatureMax));
-            $("#today-chanceRain").html(toPercent(data.daily.data[0].precipProbability));
-          });
+        newLocation = "https://api.darksky.net/forecast/c0188a72d861db401edede8a318711f6/" + lat +"," + lng;
+        alert(newLocation);
+        getWeather(newLocation);
+          // $.ajax(newLocation, {dataType: "jsonp"}).done(function(data){
+          //   $("#today-currentTemp").html(round(data.currently.temperature));
+          //   $("#today-summary").html(data.currently.summary);
+          //   $("#today-minTemp").html(round(data.daily.data[0].temperatureMin));
+          //   $("#today-maxTemp").html(round(data.daily.data[0].temperatureMax));
+          //   $("#today-chanceRain").html(toPercent(data.daily.data[0].precipProbability));
+          // });
 
       });//end googleapis
     });//end submit
@@ -63,5 +67,5 @@ $(function(){
     // for (i=1; i <= data.length; i++){
     //   $(".API-target").append("<li>"+data[i].name+"</li>");
     // }
-  });//end ajax
+
 });//end function
